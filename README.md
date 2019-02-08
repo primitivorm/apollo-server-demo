2cf8d742076751e6eca2ff6f0a2c83f82048b0a0
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2019-02-08 16:44:45 -0600

    add mutation

--- README.md
+++ README.md
@@ -6,3 +6,50 @@ Demo for apollo
 
 ## Run
 `npm run start`
+
+## Ejemplos
+
+### Query
+
+```
+query {
+clients {
+  clientId
+  clientNumber
+  taxId
+  businessName
+  commercialName
+  countryCode
+  status
+  contacts {
+    personId
+    phone
+    email
+  }
+}
+}
+```
+
+### Mutation
+
+```
+mutation {
+  addPerson(input: {
+    name: "Primitivo RM",
+    jobTitle: "Team Lead",
+    email: "primitivo@example.com",
+    phone: "55 12345678",
+    mobilePhone: "55 987654321",
+    countryCode: "cdmx",
+    country: "Mexico"
+  }) {
+    personId
+  }
+}
+
+
+
+
+
+
+```
--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -3,6 +3,22 @@ import { clients } from './query/clients'
 const resolvers = {
     Query: {
         clients
+    },
+    Mutation: {
+        addPerson({input}){
+            const uuidv1 = require('uuid/v1')
+            var uid = uuidv1()
+            return {
+                personId: uid,
+                name: input.name,
+                jobTitle: input.jobTitle,
+                email: input.email,
+                phone: input.phone,
+                mobilePhone: input.mobilePhone,
+                countryCode: input.countryCode,
+                country: input.country
+            }
+        }
     }
 }
 
